676dc52bbbf1b9ce0b5925d8a1c3ed4ae7e55ddc
ZenBoba <54600194+ZenBoba@users.noreply.github.com>
2019-11-07 13:52:03 -0700

    Update enable-matrix.js

--- enable-matrix.js
+++ enable-matrix.js
@@ -2,12 +2,12 @@ pins.analogWritePin(AnalogPin.P8, 0)
 pins.analogWritePin(AnalogPin.P12, 0)
 pins.analogWritePin(AnalogPin.P16, 0)
 basic.forever(function () {
-    for (let index = 0; index <= 1024; index++) {
+    for (let index = 0; index <= 1024; index++) { // lights up the leds continously
         basic.clearScreen()
         pins.analogWritePin(AnalogPin.P8, index)
         pins.analogWritePin(AnalogPin.P12, index)
         pins.analogWritePin(AnalogPin.P16, index)
-        if (index == 500) {
+        if (index == 500) { // shows the led matrix is displayed
             basic.plotLeds(`
                 . . . . .
                 . . . . .
