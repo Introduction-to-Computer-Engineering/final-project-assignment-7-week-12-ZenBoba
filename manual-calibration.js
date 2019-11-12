a76ea301e366728be0c398e9881c84a3512eba12
ZenBoba <54600194+ZenBoba@users.noreply.github.com>
2019-11-12 15:18:50 -0700

    Updated manual-calibration.js
    changed the js code so it lights up the bar / row of leds instead of singular led

--- manual-calibration.js
+++ manual-calibration.js
@@ -3,11 +3,18 @@ basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P8, 1)//power on
     moist = pins.analogReadPin(AnalogPin.P0)//SIG
     pins.digitalWritePin(DigitalPin.P8, 0)//power off
-    let scalelevel = pins.map(moist, 50, 840, 0, 4)
-    led.plot(0, scalelevel) // adjust the led to read level of wetness.
-    //the lower the led goes, the more wet the reading is
+    let scalelevel = pins.map(moist, 50, 840, 4, 0)
+    for (let x = 0; x < 5; x++) { // portion of the led matrix light up depending on wetness
+        for (let y = 5; y > scalelevel; y--) {
+            led.plot(x, y)
+        }
+    }
     basic.pause(1000)
-    led.unplot(0, scalelevel) // adjusts to remove the led after reading the level of wetness
+    for (let x = 0; x < 5; x++) { // unplots the leds allowing another reading to be done
+        for (let y = 5; y > scalelevel; y--) {
+            led.unplot(x, y)
+        }
+    }
 })
 // dry 50 reading
 // wet 840 reading
